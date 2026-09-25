cf3f175d428309552cdc52fd63a596b76aa06929
Ericyiyou <ericyou100@gmail.com>
2026-09-25 15:13:52 +0800

    implement costmap and control nodes

--- src/robot/costmap/include/costmap_node.hpp
+++ src/robot/costmap/include/costmap_node.hpp
@@ -1,8 +1,11 @@
+// costmap_node.hpp
 #ifndef COSTMAP_NODE_HPP_
 #define COSTMAP_NODE_HPP_
 
+#include <vector>
 #include "rclcpp/rclcpp.hpp"
-
+#include "sensor_msgs/msg/laser_scan.hpp"
+#include "nav_msgs/msg/occupancy_grid.hpp"
 #include "costmap_core.hpp"
 
 class CostmapNode : public rclcpp::Node {
@@ -10,7 +13,24 @@ class CostmapNode : public rclcpp::Node {
     CostmapNode();
 
   private:
+    void laserCallback(const sensor_msgs::msg::LaserScan::SharedPtr scan);
+    void initializeCostmap();
+    void convertToGrid(double range, double angle, int &x_grid, int &y_grid);
+    void markObstacle(int x_grid, int y_grid);
+    void inflateObstacles();
+    void publishCostmap();
+
     robot::CostmapCore costmap_;
+    rclcpp::Subscription<sensor_msgs::msg::LaserScan>::SharedPtr lidar_sub_;
+    rclcpp::Publisher<nav_msgs::msg::OccupancyGrid>::SharedPtr costmap_pub_;
+
+    double resolution_;
+    int width_, height_;
+    double origin_x_, origin_y_;
+    std::vector<std::vector<int8_t>> grid_;
+
+    double inflation_radius_;
+    int8_t max_cost_;
 };
 
-#endif 
+#endif
